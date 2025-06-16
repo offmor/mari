ad458e6585c0d00040b088aa8d1dedd7f62c9d8a
Geovane Fedrecheski <geonnave@gmail.com>
2025-06-16 20:39:18 +0200

    feat: handle node join and leave

--- marilib/examples/basic.py
+++ marilib/examples/basic.py
@@ -1,22 +1,27 @@
 import time
-from mira_edge.mira_edge import MiraEdge
+from mira_edge.mira_edge import MiraEdge, EdgeEvent
 from mira_edge.protocol import Frame, MIRA_BROADCAST_ADDRESS
 
 
-def on_receive(frame: Frame):
-    print(f"Received frame: {frame}")
+def on_event(event: EdgeEvent, data: Frame | bytes):
+    if event == EdgeEvent.NODE_JOINED:
+        print(f"Node {data} joined")
+    elif event == EdgeEvent.NODE_LEFT:
+        print(f"Node {data} left")
+    elif event == EdgeEvent.NODE_DATA:
+        print(f"Got frame from {data.header.source}: {data.payload}")
 
-mira = MiraEdge(on_receive=on_receive)
+mira = MiraEdge(on_event=on_event)
 mira.connect_to_gateway() # via UART
 
-for i in range(3):
-    payload = b"aaa aaa aaa"
-    print(f"Sending broadcast frame: {payload}")
-    mira.send_frame(dst=MIRA_BROADCAST_ADDRESS, payload=payload)
+for i in range(30):
+    # payload = b"aaa aaa aaa"
+    # print(f"Sending broadcast frame: {payload}")
+    # mira.send_frame(dst=MIRA_BROADCAST_ADDRESS, payload=payload)
     time.sleep(1)
 
-    # for node in mira.nodes:
-    #     mira.send_frame(dst=node.address, payload=b"Hello, World!")
+    for node in mira.nodes:
+        mira.send_frame(dst=node, payload=b"Hello, World!")
 
 
 mira.disconnect_from_gateway()
--- marilib/mira_edge/mira_edge.py
+++ marilib/mira_edge/mira_edge.py
@@ -1,11 +1,21 @@
 from typing import Callable
+from enum import IntEnum
 from mira_edge.protocol import Frame, Header, ProtocolPayloadParserException
 from mira_edge.serial_adapter import SerialAdapter
 from mira_edge.serial_interface import SerialInterfaceException
 
+
+class EdgeEvent(IntEnum):
+    """Types of UART packet."""
+    NODE_JOINED = 1
+    NODE_LEFT = 2
+    NODE_DATA = 3
+
+
 class MiraEdge:
-    def __init__(self, on_receive: Callable[[Frame], None], port: str = "/dev/ttyACM0", baudrate: int = 1_000_000):
-        self.on_receive = on_receive
+    def __init__(self, on_event: Callable[[EdgeEvent, Frame | bytes], None], port: str = "/dev/ttyACM0", baudrate: int = 1_000_000):
+        self.on_event = on_event
+        self.nodes = []
         try:
             self._interface = SerialAdapter(
                 port, baudrate
@@ -17,11 +27,25 @@ class MiraEdge:
             print(f"Error: {exc}")
 
     def on_data_received(self, data: bytes):
-        try:
-            frame = Frame().from_bytes(data)
-            self.on_receive(frame)
-        except (ValueError, ProtocolPayloadParserException) as exc:
-            print(f"Failed to decode frame: {exc}")
+        if data[0] == EdgeEvent.NODE_JOINED:
+            print(f"Node joined: {data[1:9]}")
+            self.nodes.append(data[1:9])
+            self.on_event(EdgeEvent.NODE_JOINED, data[1:9])
+        elif data[0] == EdgeEvent.NODE_LEFT:
+            print(f"Node left: {data[1:9]}")
+            try:
+                self.nodes.remove(data[1:9])
+                self.on_event(EdgeEvent.NODE_LEFT, data[1:9])
+            except ValueError:
+                print(f"Node {data[1:9]} not found in nodes list")
+        elif data[0] == EdgeEvent.NODE_DATA:
+            try:
+                frame = Frame().from_bytes(data)
+                self.on_event(EdgeEvent.NODE_DATA, frame)
+            except (ValueError, ProtocolPayloadParserException) as exc:
+                print(f"Failed to decode frame: {exc}")
+        else:
+            print(f"Unknown event: {data[0]} -- {data}")
 
     def connect_to_gateway(self):
         self._interface.init(self.on_data_received)
