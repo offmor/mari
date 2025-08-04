73cc9049e10f748617791ba7ec012382499deaf3
Geovane Fedrecheski <geonnave@gmail.com>
2025-08-04 17:22:16 +0200

    chore: run hatch fmt

--- marilib/marilib/model.py
+++ marilib/marilib/model.py
@@ -200,7 +200,9 @@ class GatewayInfo(Packet):
             PacketFieldMetadata(name="address", length=8),
             PacketFieldMetadata(name="network_id", length=2),
             PacketFieldMetadata(name="schedule_id", length=1),
-            PacketFieldMetadata(name="schedule_stats", length=4*8), # 4 uint64_t values
+            PacketFieldMetadata(
+                name="schedule_stats", length=4 * 8
+            ),  # 4 uint64_t values
         ]
     )
     address: int = 0
@@ -211,8 +213,8 @@ class GatewayInfo(Packet):
     def repr_schedule_stats(self):
         if not self.schedule_stats:
             return ""
-        all_bits = format(self.schedule_stats, f'0{4*8*8}b')
-        all_bits = [all_bits[i:i+8] for i in range(0, len(all_bits), 8)]
+        all_bits = format(self.schedule_stats, f"0{4*8*8}b")
+        all_bits = [all_bits[i : i + 8] for i in range(0, len(all_bits), 8)]
         all_bits.reverse()
         # print(">>>", reversed(all_bits[0].split("")))
         all_bits = [list(reversed(bits)) for bits in all_bits]
