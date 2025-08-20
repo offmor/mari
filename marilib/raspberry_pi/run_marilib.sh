aaa5d0146c62aa524376a9cc5782007e38681a0f
nermine11 <narminlkilani@gmail.com>
2025-08-20 12:20:13 +0200

    chore: modify the systemd service to use port ttyACM10 (#43)

--- marilib/raspberry_pi/run_marilib.sh
+++ marilib/raspberry_pi/run_marilib.sh
@@ -13,24 +13,36 @@ cat <<'EOF' | sudo tee /etc/systemd/system/run_marilib.service
 [Unit]
 Description=run marilib
 #If the gateway disconnects, stop the service
-BindsTo=dev-ttyACM0.device
-After=dev-ttyACM0.device
+BindsTo=dev-ttyACM10.device
+After=dev-ttyACM10.device
 
 [Service]
 User=pi
 WorkingDirectory=/home/pi/marilib
 
 #refuse to start if the gateway device is missing
-ConditionPathExists=/dev/ttyACM0
-ExecStartPre=/usr/bin/test -e /dev/ttyACM0
+ConditionPathExists=/dev/ttyACM10
+ExecStartPre=/usr/bin/test -e /dev/ttyACM10
 
 # run basic.py
-ExecStart=/usr/bin/tmux new-session -A -s marilib -d  "/home/pi/marilib/venv/bi>
+ExecStart=/usr/bin/tmux new-session -A -s marilib -d  "/home/pi/marilib/venv/bin/python /home/pi/marilib/examples/basic.py --p /dev/ttyACM10"
 ExecStop=/usr/bin/tmux kill-session -t marilib
 Type=forking
 
 Restart=on-failure
 RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# === Step 4:  create a path unit that triggers when the gateway device appears ===
+cat <<'EOF' | sudo tee /etc/systemd/system/run_marilib.path
+[Unit]
+Description=Launch marilib when the gateway appears on /dev/ttyACM10
+
+[Path]
+PathExists=/dev/ttyACM10
 
 [Install]
 WantedBy=multi-user.target
