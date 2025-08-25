671f510ba65f59c3d776e05d032699ba2bc69e86
nermine11 <narminlkilani@gmail.com>
2025-08-25 17:13:28 +0200

    chore: change service name to setup_marilib_service (#43)

--- marilib/examples/raspberry-pi/setup_marilib_service.sh
+++ marilib/examples/raspberry-pi/setup_marilib_service.sh
@@ -11,10 +11,10 @@ sudo chmod +x bind_interface.sh
 source bind_interface.sh
 
 # === Step 4: create the service to run marilib on boot once the gateway is connected ===
-cat <<'EOF' | sudo tee /etc/systemd/system/run_marilib.service
+cat <<'EOF' | sudo tee /etc/systemd/system/setup_marilib_service.service
 
 [Unit]
-Description=run marilib
+Description=run marilib on boot
 #If the gateway disconnects, stop the service
 
 [Service]
@@ -24,6 +24,7 @@ WorkingDirectory=/home/pi/marilib
 #refuse to start if the gateway device is missing
 ConditionPathExists=/dev/ttyACM10
 ExecStartPre=/bin/sleep 5
+
 #check that ttyACM10 is available not just that it exists
 ExecStartPre=/usr/bin/udevadm settle
 ExecStartPre=/bin/bash -c 'for i in {1..200};do exec 3<>/dev/ttyACM10 && exit 0 || sleep 0.2; done; echo " Gateway port: ttyACM10 not ready" >&2; exit 1'
@@ -41,7 +42,7 @@ WantedBy=multi-user.target
 EOF
 
 # === Step 5:  create a path unit that triggers when the gateway device appears ===
-cat <<'EOF' | sudo tee /etc/systemd/system/run_marilib.path
+cat <<'EOF' | sudo tee /etc/systemd/system/setup_marilib_service.path
 [Unit]
 Description=Launch marilib when the gateway appears on /dev/ttyACM10
 
@@ -54,5 +55,5 @@ EOF
 
 # reload systemd units and enable
 sudo systemctl daemon-reload
-sudo systemctl enable --now run_marilib.path
-sudo systemctl enable run_marilib.service
+sudo systemctl enable --now setup_marilib_service.path
+sudo systemctl enable setup_marilib_service.service
