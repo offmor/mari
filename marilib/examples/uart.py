4585de0d63a5fa606aac9135478c0405886a727f
Geovane Fedrecheski <geonnave@gmail.com>
2025-09-16 10:09:42 +0200

    uart: WIP adjust for bulk tx with trigger byte

--- marilib/examples/uart.py
+++ marilib/examples/uart.py
@@ -9,7 +9,7 @@ from marilib.serial_hdlc import (
 from marilib.serial_uart import SerialInterface
 
 BAUDRATE = 1000000
-# BAUDRATE = 115200
+# BAUDRATE = 460_800
 
 hdlc_handler = HDLCHandler()
 
@@ -30,13 +30,42 @@ serial_interface = SerialInterface("/dev/ttyACM0", BAUDRATE, on_byte_received)
 
 
 while True:
-    time.sleep(1)
-    payload = b"A" * 4
-    print(f"Sending {len(payload)} bytes: {payload.hex(' ')}")
-    encoded = hdlc_encode(payload)
-    print(f"Sending encoded {len(encoded)} bytes: {encoded.hex(' ')}")
-    serial_interface.write(encoded[0:1])
-    time.sleep(0.01)
-    serial_interface.write(encoded[1:])
+
+    def send_payload(payload):
+        print(f"\nSending {len(payload)} bytes: {payload.hex(' ')}")
+        encoded = hdlc_encode(payload)
+        print(f"Sending encoded {len(encoded)} bytes: {encoded.hex(' ')}")
+
+        serial_interface.write_chunked_with_trigger_byte(encoded)
+        # serial_interface.write(encoded[0:1])
+        # time.sleep(0.0001)  # 100 us -- this time is important because of the trigger byte timeout on the nRF side
+        # serial_interface.write(encoded[1:])
+
+
+    # sleep_time = 0.1 # 100 ms
+    sleep_time = 0.01 # 10 ms
+
+    # send_payload(b"A" * 1)
+    # time.sleep(sleep_time)
+    # send_payload(b"ABCD" * 4)
+    # time.sleep(sleep_time)
+    # send_payload(b"ABCD" * 4) # 16 bytes
+    # time.sleep(sleep_time)
+    # send_payload(b"ABCD" * 8) # 32 bytes
+    # time.sleep(sleep_time)
+    # send_payload(b"ABCD" * 16) # 64 bytes
+    # time.sleep(sleep_time)
+    # send_payload(b"ABCD" * 32) # 128 bytes
+    # time.sleep(sleep_time)
+    # send_payload(b"ABCD" * 64) # 256 bytes
+
+    for i in range(4):
+        # send_payload(b"ABCD" * 8) # 32 bytes
+        # send_payload(b"ABCD" * 16) # 64 bytes
+        send_payload(b"A" * 80)
+        time.sleep(sleep_time)
 
     # serial_interface.write(b"A" * 8)
+
+    print("Sleeping for 2 seconds\n\n")
+    time.sleep(0.01)
